d2310421729a4bc08942daa1401dedfacd6cea7c
NexenFull14 <100863416+NexenFull14@users.noreply.github.com>
2023-04-26 19:14:02 +0200

    Replaced all necessary css parts for all pages in styles.css and added #reset button to all pages.

--- js/script.js
+++ js/script.js
@@ -17,26 +17,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
       resetButton.style.display = "none";
     }
   });
-
-  const toggleCheckbox = document.getElementById("theme-toggle");
-  const body = document.body;
-
-  //Vraca dark/light mode na pocetak
-  const userPref = localStorage.getItem("theme");
-
-  if (userPref === "dark") {
-    toggleCheckbox.checked = true;
-    body.classList.add("dark-mode");
-  }
-
-  toggleCheckbox.addEventListener("change", function () {
-    body.classList.toggle("dark-mode");
-
-    // Cuva dark/light mode u local storage
-    if (body.classList.contains("dark-mode")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
-  });
+  
 });
