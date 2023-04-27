d06b1b28a76c77f060ce635857b7241a56a03352
NexenFull14 <100863416+NexenFull14@users.noreply.github.com>
2023-04-27 23:41:12 +0200

    Added some JavaScript code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const resetButton = document.getElementById("reset");
+  const resetButton = document.querySelector("#reset");
+  resetButton.style.opacity = '0';
 
   resetButton.addEventListener("click", () => {
     window.scrollTo({
       top: 0,
-      left: 0,
       behavior: "smooth",
+      duration: 4000,
     });
   });
 
   window.addEventListener("scroll", () => {
     if(window.pageYOffset > 300) {
       resetButton.style.display = "block";
+      resetButton.style.transition = "all 4s linear";
+      resetButton.style.opacity = "1";
     }
     else {
       resetButton.style.display = "none";
+      resetButton.style.opacity = "0";
     }
   });
   
